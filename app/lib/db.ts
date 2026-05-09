16a28fb67bf879d00d526ad241d772eac3f2eec4
Vignesh <wvignesh983@gmail.com>
2026-05-09 06:33:17 +0530

    fix(seceret): moved db secrets to env

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12825797",
-  password: "VRqpThB4vg",
-  database: "sql12825797",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
